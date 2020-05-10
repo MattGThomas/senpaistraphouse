ebbc2565850e0fd76e26edbf7cd9fd5960b637e3
MattGThomas <mgthomas0616@gmail.com>
2020-05-10 14:15:48 -0500

    finished videos page

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -5,26 +5,26 @@ import "./videos.css";
 class Videos extends Component {
   render() {
     return (
-      <MDBContainer>
-        <div>
-          <h2 style={{ color: "white" }} className="traphouse-pod">
-            Welcome To The Traphouse Podcast
-          </h2>
-          <MDBRow className="videos-header" style={{ paddingTop: "5%" }}>
-            <MDBCol style={{ width: "100%" }}>
-              <div>
-                <p style={{ color: "#FFFFFF" }}>
-                  SENPAI'S TRAPHOUSE EP. 1: PILOT
-                </p>
-              </div>
-
-              <ReactPlayer url="https://www.youtube.com/watch?v=bZ_Ye4Rz6Ws" />
+      <div>
+        <MDBContainer>
+          <div className="videos">
+            <h2 style={{ color: "white" }}>WELCOME TO THE TRAPHOUSE PODCAST</h2>
+          </div>
+          <MDBRow>
+            <div>
+              <p style={{ color: "#FFFFFF" }} className="text-center">
+                SENPAI'S TRAPHOUSE EP. 1: PILOT
+              </p>
+            </div>
+          </MDBRow>
+          <MDBRow className="stupid-video">
+            <MDBCol>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=bZ_Ye4Rz6Ws"
+                width="100%"
+              />
             </MDBCol>
-
-            <MDBCol
-              className="d-flex flex-column"
-              style={{ paddingBottom: "16%", width: "100%" }}
-            >
+            <MDBCol className="text-left" style={{ paddingBottom: "16%" }}>
               <span className="p-2 text-left">
                 A round table style discussion covering a variety of anime
                 related topics
@@ -34,18 +34,59 @@ class Videos extends Component {
               <span className="p-2 text-left">
                 -Sake and a snack: Dorayaki, Mochi, Coffee, Pocky, and Sake
               </span>
-
+              <br />
               <span className="p-2 text-left">
                 -Icebreaker: how did you get into anime
               </span>
-
+              <br />
               <span className="p-2 text-left">
                 -Main Topic: "Good and Bad anime fathers"
               </span>
             </MDBCol>
           </MDBRow>
-        </div>
-      </MDBContainer>
+        </MDBContainer>
+      </div>
+      // <MDBContainer>
+      //   <div>
+      //     <h2 style={{ color: "white" }} className="traphouse-pod">
+      //       Welcome To The Traphouse Podcast
+      //     </h2>
+      //     <MDBRow className="videos-header" style={{ paddingTop: "5%" }}>
+      //       <MDBCol style={{ width: "100%" }}>
+      //         <div>
+      //           <p style={{ color: "#FFFFFF" }}>
+      //             SENPAI'S TRAPHOUSE EP. 1: PILOT
+      //           </p>
+      //         </div>
+
+      //         <ReactPlayer url="https://www.youtube.com/watch?v=bZ_Ye4Rz6Ws" />
+      //       </MDBCol>
+
+      //       <MDBCol
+      //         className="d-flex flex-column"
+      //         style={{ paddingBottom: "16%", width: "100%" }}
+      //       >
+      //         <span className="p-2 text-left">
+      //           A round table style discussion covering a variety of anime
+      //           related topics
+      //         </span>
+
+      //         <br />
+      //         <span className="p-2 text-left">
+      //           -Sake and a snack: Dorayaki, Mochi, Coffee, Pocky, and Sake
+      //         </span>
+
+      //         <span className="p-2 text-left">
+      //           -Icebreaker: how did you get into anime
+      //         </span>
+
+      //         <span className="p-2 text-left">
+      //           -Main Topic: "Good and Bad anime fathers"
+      //         </span>
+      //       </MDBCol>
+      //     </MDBRow>
+      //   </div>
+      // </MDBContainer>
     );
   }
 }
